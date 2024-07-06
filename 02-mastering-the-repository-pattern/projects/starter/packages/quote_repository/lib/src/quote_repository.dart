15cda5d1821f0c776db39b3d464266f0a67d96b7
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-07-06 20:27:12 +0300

    11. Handle Class Dependencies in WonderWords

--- 02-mastering-the-repository-pattern/projects/starter/packages/quote_repository/lib/src/quote_repository.dart
+++ 02-mastering-the-repository-pattern/projects/starter/packages/quote_repository/lib/src/quote_repository.dart
@@ -6,8 +6,43 @@ import 'package:quote_repository/src/mappers/mappers.dart';
 import 'package:quote_repository/src/quote_local_storage.dart';
 
 class QuoteRepository {
+  // 1. Add constructor and data sources properties.
 
-  // TODO: Add constructor and data sources properties.
+  //* There are a couple of things to note in there:
+  //* First, take a look at the two final properties. As mentioned, repositories
+  //* orchestrate multiple data sources. In this case, you have two:
+  //* 1. FavQsApi: Retrieves and sends data to your remote API. FavQsApi comes
+  //* from another internal package of this project: fav_qs_api.
+  //* 2. QuoteLocalStorage: Retrieves and stores quotes in the device’s local storage.
+  //* QuoteLocalStorage does not come from a separate package. It’s defined
+  //* inside this same folder you’re working on.
+
+  //? The first question you might have is: Why does FavQsApi have its own package
+  //? while QuoteLocalStorage doesn’t?
+  //! QuoteLocalStorage is more specialized because it only deals with quotes.
+  //! Therefore, it has no utility outside the quote_repository package.
+
+  //! FavQsApi , on the other hand, is more generic because it handles both quotes
+  //! and authentication calls. That makes it suitable for the user_repository
+  //! package as well, which you’ll cover in Chapter 6, “Authenticating Users”. As you
+  //! know, when you need to share code between two packages — in this case, two
+  //! repositories — you have to create a third one. In this case, that’s fav_qs_api.
+
+  //* Although QuoteLocalStorage doesn’t come from a separate package, it
+  //* depends on KeyValueStorage, which does come from the separate
+  //* key_value_storage package.
+  //* Think of KeyValueStorage as WonderWords’ local database. It’s a wrapper
+  //* around the popular Hive package. It had to become an internal package of its
+  //* own to concentrate all of the Hive configuration in a single place.
+  final FavQsApi remoteApi;
+  final QuoteLocalStorage _localStorage;
+
+  QuoteRepository({
+    required KeyValueStorage keyValueStorage,
+    required this.remoteApi,
+    @visibleForTesting QuoteLocalStorage? localStorage,
+  }) : _localStorage =
+            localStorage ?? QuoteLocalStorage(keyValueStorage: keyValueStorage);
 
   Stream<QuoteListPage> getQuoteListPage(
     int pageNumber, {
