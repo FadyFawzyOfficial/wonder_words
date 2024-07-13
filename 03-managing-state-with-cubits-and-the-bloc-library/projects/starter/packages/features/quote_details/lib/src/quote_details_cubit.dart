17c6c96c2f4f3e1aa8edbf1a446ea0478f56bddb
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-07-13 21:04:59 +0300

    3. Create a Cubit (QuoteDetailsCubit)

--- 03-managing-state-with-cubits-and-the-bloc-library/projects/starter/packages/features/quote_details/lib/src/quote_details_cubit.dart
+++ 03-managing-state-with-cubits-and-the-bloc-library/projects/starter/packages/features/quote_details/lib/src/quote_details_cubit.dart
@@ -5,4 +5,51 @@ import 'package:quote_repository/quote_repository.dart';
 
 part 'quote_details_state.dart';
 
-// TODO: Create the cubit.
+//! Creating a Cubit
+
+// 1. To create a Cubit, you have to extend Cubit and specify your base state class
+// as the generic type. The only reason you're able to import the Cubit class in
+// this file is because this quote_details packages's pubspec.yaml lists flutter_bloc
+// as a dependency.
+class QuoteDetailsCubit extends Cubit<QuoteDetailsState> {
+  final int quoteId;
+  //? 3. You'll use the QuoteRepository you crated in the previous.
+  final QuoteRepository quoteRepository;
+
+  QuoteDetailsCubit({
+    required this.quoteId,
+    required this.quoteRepository,
+  }) : super(
+          //* 2. When extending Cubit, you have to call the super constructor and
+          //* pass an instance olf your initial state to it. This value is what
+          //* the Cubit will provide to the UI when the screen first opens.
+          const QuoteDetailsInProgress(),
+        );
+
+  void _fetchQuoteDetails() async {
+    // TODO: Featch data from QuoteRepository.
+  }
+
+  void refetch() async {
+    // TODO: Add a body to refetch().
+  }
+
+  void upvoteQuote() async {
+    // TODO: Add a body to upvoteQuote().
+  }
+
+  void downvoteQuote() async {
+    // TODO: Challenge.
+  }
+
+  void unvoteQuote() async {
+    // TODO: Challenge.
+  }
+
+  void favoriteQuote() async {
+    // TODO: Challenge.
+  }
+  void unfavoriteQuote() async {
+    // TODO: Challenge.
+  }
+}
