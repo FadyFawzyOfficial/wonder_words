6a5ab6d782989fa77f901dd48a4184cb382eae7c
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-02-26 22:25:34 +0200

    3. Create the app's routing table with use CupertinoTabPage to achieve nested navigation

--- 07-routing-and-navigating/projects/starter/lib/routing_table.dart
+++ 07-routing-and-navigating/projects/starter/lib/routing_table.dart
@@ -13,7 +13,40 @@ import 'package:update_profile/update_profile.dart';
 import 'package:user_repository/user_repository.dart';
 import 'package:wonder_words/tab_container_screen.dart';
 
-// TODO: Create the app's routing table.
+// Completed: Create the app's routing table.
+//! 1. The return type is a Map<String, PageBuilder> . As you’ve seen, this maps
+//! each path you want to support — the String — to the function that builds
+//! the corresponding page — the PageBuilder .
+Map<String, PageBuilder> buildRoutingTable({
+  //* 2. Most of the dependencies you’ll need to instantiate your screens are already
+  //* available on lib/main.dart, so you’re asking them to be passed onto this
+  //* function so you can reuse them.
+  required RoutemasterDelegate routeDelegate,
+  required UserRepository userRepository,
+  required QuoteRepository quoteRepository,
+  required RemoteValueService remoteValueService,
+  required DynamicLinkService dynamicLinkService,
+}) {
+  return {
+    //! 3. The first path you declared is your app’s entry point: the '/'. The screen
+    //! you’re assigning to this path is the TabContainerScreen you’ve created.
+    //! This is the outermost screen that will hold the bottom
+    //! tab along with the two nested navigation stacks.
+    _PathConstants.tabContainerPath: (_) =>
+        //! 4. To achieve the nested navigation layout, you wrapped your
+        //! TabContainerScreen widget inside a CupertinoTabPage class. You then
+        //! leveraged its paths parameter to define which two routes should be the
+        //! entry point for each internal flow.
+        CupertinoTabPage(
+          child: const TabContainerScreen(),
+          paths: [
+            _PathConstants.quoteListPath,
+            _PathConstants.profileMenuPath,
+          ],
+        ),
+    // TODO: Define the two nested routes homes.
+  };
+}
 
 // Completed: Define the app's paths.
 class _PathConstants {
