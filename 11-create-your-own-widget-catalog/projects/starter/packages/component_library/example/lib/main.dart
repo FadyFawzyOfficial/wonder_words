21fe7f6a460520af7b07b15a4fa61a4791adf24a
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-06-09 19:11:20 +0300

    1. Create the main.dart file of component library package example

---
+++ 11-create-your-own-widget-catalog/projects/starter/packages/component_library/example/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+// ToDo: add missing import
+
+void main() {
+  runApp(
+    // ToDo: replace the MaterialApp placeholder later
+    MaterialApp(
+      home: Container(
+        color: Colors.grey,
+      ),
+    ),
+  );
+}
