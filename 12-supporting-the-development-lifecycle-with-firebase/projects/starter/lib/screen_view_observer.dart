45ef4c3746ff4297071cc16485e4f42669d9ecd8
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-06-12 12:06:23 +0300

    2. Override didPush and didPop of RoutemasterObserver (ScreenObserver) methods to track screen views

--- 12-supporting-the-development-lifecycle-with-firebase/projects/starter/lib/screen_view_observer.dart
+++ 12-supporting-the-development-lifecycle-with-firebase/projects/starter/lib/screen_view_observer.dart
@@ -3,11 +3,9 @@ import 'package:monitoring/monitoring.dart';
 import 'package:routemaster/routemaster.dart';
 
 class ScreenViewObserver extends RoutemasterObserver {
-  ScreenViewObserver({
-    required this.analyticsService,
-  });
-
   final AnalyticsService analyticsService;
+
+  ScreenViewObserver({required this.analyticsService});
 
   // Completed: add _sendScreenView() helper method
   void _sendScreenView(PageRoute<dynamic> route) {
@@ -21,5 +19,22 @@ class ScreenViewObserver extends RoutemasterObserver {
     }
   }
 
-  // TODO: override didPush and didPop method
+  // Completed: override didPush and didPop method
+  //! When navigating to a new screen, the didPush method passes its route to your _sendScreenView method.
+  @override
+  void didPush(Route route, Route? previousRoute) {
+    super.didPush(route, previousRoute);
+    if (route is PageRoute) _sendScreenView(route);
+  }
+
+  //! When navigating back to the previous screen, the current screen disappears,
+  //! and the previous screen reappears. That’s when the didPop method passes
+  //! the previous route to the _sendScreenView method instead of the current route.
+  @override
+  void didPop(Route route, Route? previousRoute) {
+    super.didPop(route, previousRoute);
+    if (previousRoute is PageRoute && route is PageRoute) {
+      _sendScreenView(previousRoute);
+    }
+  }
 }
