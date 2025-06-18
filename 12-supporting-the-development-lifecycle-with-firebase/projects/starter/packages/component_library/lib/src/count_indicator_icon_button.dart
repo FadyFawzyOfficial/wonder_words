64ee747b64657eeebf765420d62501eae0b072aa
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-06-18 11:33:37 +0300

    3. Produce a Flutter Framework Error & Handle it (How to)

--- 12-supporting-the-development-lifecycle-with-firebase/projects/starter/packages/component_library/lib/src/count_indicator_icon_button.dart
+++ 12-supporting-the-development-lifecycle-with-firebase/projects/starter/packages/component_library/lib/src/count_indicator_icon_button.dart
@@ -33,8 +33,8 @@ class CountIndicatorIconButton extends StatelessWidget {
           Text(
             count.toString(),
             style: const TextStyle(
-              // TODO: change the font size for invoking an error
-              fontSize: FontSize.small,
+              // Completed: change the font size for invoking an error
+              fontSize: FontSize.xxLarge,
             ),
           ),
         ],
