1c429f16965f4a37b8da6941715d794e90802002
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-06-21 22:27:52 +0300

    2. Build an implementation of RemoteValueService (Wrapper around Firebase Remote Config)

--- 13-running-live-experiments-with-a-b-testing-and-feature-flags/project/starter/packages/monitoring/lib/src/remote_value_service.dart
+++ 13-running-live-experiments-with-a-b-testing-and-feature-flags/project/starter/packages/monitoring/lib/src/remote_value_service.dart
@@ -1,7 +1,26 @@
+// Completed: import firebase_remote_config package
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/foundation.dart';
-// TODO: import firebase_remote_config package
 
 /// Wrapper around [FirebaseRemoteConfig].
 class RemoteValueService {
-// TODO: add an implementation of RemoteValueService class
+  // Completed: add an implementation of RemoteValueService class
+  static const _gridQuotesViewEnabledKey = 'grid_quotes_view_enabled';
+
+  final FirebaseRemoteConfig _remoteConfig;
+
+  //* 1. Initializes the instance of FirebaseRemoteConfig.
+  RemoteValueService({@visibleForTesting FirebaseRemoteConfig? remoteConfig})
+      : _remoteConfig = remoteConfig ?? FirebaseRemoteConfig.instance;
+
+  //! 2. Fetches and activates the configuration that you’ll set up on the
+  //! Firebase Remote Config console.
+  Future<void> load() async {
+    // TODO: add default values for your parameters
+    await _remoteConfig.fetchAndActivate();
+  }
+
+  //* 3. Returns the value of the feature flag you defined.
+  bool get isGridQuotesViewEnabled =>
+      _remoteConfig.getBool(_gridQuotesViewEnabledKey);
 }
