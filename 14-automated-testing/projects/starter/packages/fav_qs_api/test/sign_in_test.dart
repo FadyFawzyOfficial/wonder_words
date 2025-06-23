9b001a04c917b6f8a812b9c46d19d8eda73befd8
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-06-23 14:28:53 +0300

    2. Refactor sign_in_test.dart: Enhance comments for clarity and understanding of test steps

--- 14-automated-testing/projects/starter/packages/fav_qs_api/test/sign_in_test.dart
+++ 14-automated-testing/projects/starter/packages/fav_qs_api/test/sign_in_test.dart
@@ -9,16 +9,18 @@ void main() {
   test(
       'When sign in call completes successfully, returns an instance of UserRM',
       () async {
-    // 1
+    //* 1. Initializes an instance of the Dio object, which is required to preform
+    //* HTTP requests.
     final dio = Dio(BaseOptions());
 
     // TODO: add dioAdapter which will stub the expected response of remote API
 
-    // 2
+    //* 2. Initialize the remote API and provides the required testing attributes.
     final remoteApi =
         FavQsApi(userTokenSupplier: () => Future.value(), dio: dio);
 
-    // 3
+    //* 3. Initializes all the required variables, which are used when performing
+    //* the sign-in request.
     const email = 'email';
     const password = 'password';
 
@@ -33,7 +35,7 @@ void main() {
 
     // TODO: add an implementation of request stubbing
 
-    // 4
+    //! 4. Evaluates if the tested function returns the correct output.
     expect(await remoteApi.signIn(email, password), isA<UserRM>());
   });
 }
