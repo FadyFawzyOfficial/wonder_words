ce5354e13ee8d33737af1e551057578ec611b70e
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-06-23 11:23:46 +0300

    1. Add an implementation for UserRepository.getUserToken() test

--- 14-automated-testing/projects/starter/packages/user_repository/test/user_repository_test.dart
+++ 14-automated-testing/projects/starter/packages/user_repository/test/user_repository_test.dart
@@ -1,6 +1,16 @@
 // TODO: add missing packages and an annotation to generate the mock
+import 'package:test/test.dart';
+
 void main() {
-  // TODO: add an implementation for UserRepository.getUserToken() test
+  // Completed: add an implementation for UserRepository.getUserToken() test
+  test(
+    'When calling getUserToken after successful authentication, return authentication token',
+    () async {
+      // ToDo: add initialization of _userRepository
+
+      expect(await _userRepository.getUserToken(), 'token');
+    },
+  );
 
   // Challenge
 }
