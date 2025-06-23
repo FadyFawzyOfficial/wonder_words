992d834ec8989547fe8b2d9de39f9bac3d41d443
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-06-23 11:30:02 +0300

    2. Add initialization of _userRepository

--- 14-automated-testing/projects/starter/packages/user_repository/test/user_repository_test.dart
+++ 14-automated-testing/projects/starter/packages/user_repository/test/user_repository_test.dart
@@ -1,16 +1,36 @@
 // TODO: add missing packages and an annotation to generate the mock
+import 'package:fav_qs_api/fav_qs_api.dart';
+import 'package:key_value_storage/key_value_storage.dart';
 import 'package:test/test.dart';
+import 'package:user_repository/user_repository.dart';
 
 void main() {
   // Completed: add an implementation for UserRepository.getUserToken() test
   test(
     'When calling getUserToken after successful authentication, return authentication token',
     () async {
-      // ToDo: add initialization of _userRepository
+      // ToDo: add initialization of _userSecureStorage
+
+      // Completed: add initialization of _userRepository
+      //* UserRepository's constructor requires two parameters — noSqlStorage and remoteApi.
+      //! These don’t play any role when executing getUserToken(). But if you go
+      //! to the implementation of this class, you’ll see that you can also provide
+      //! the secureStorage attribute when testing this repository. This one, on the
+      //! other hand, plays a huge role when executing getUserToken().
+      //* As mentioned before, it’s crucial when performing unit tests to prevent
+      //* any unexpected behavior of other units with which the testing component interacts.
+      //! To take control over the behavior of this object, you’ll make a mock for UserSecureStorage.
+      final _userRepository = UserRepository(
+        // secureStorage: _userSecureStorage,
+        noSqlStorage: KeyValueStorage(),
+        remoteApi: FavQsApi(userTokenSupplier: () => Future.value()),
+      );
 
       expect(await _userRepository.getUserToken(), 'token');
     },
   );
+
+  // TODO: add stubbing for fetching token from secure storage
 
   // Challenge
 }
